4b4ecc1bbcd0eda7570e6658a930cc29917c467b
Leonel Mandarino <leonelmandarino@gmail.com>
2019-03-16 10:43:54 -0300

    Add 'home' app

--- is2/settings.py
+++ is2/settings.py
@@ -33,6 +33,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'home.apps.HomeConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
