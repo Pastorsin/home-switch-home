528ea6a4c6c86acc0f09f449f8a7825634b1b52e
Andrés Milla <andressmilla@gmail.com>
2019-03-24 09:24:15 -0300

    Add 'apps' dir

--- is2/settings.py
+++ is2/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
 import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Insert apps dir
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -35,7 +38,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     'home.apps.HomeConfig',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
