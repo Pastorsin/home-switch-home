b323685a29c74f730d59539494e416895a27761b
Andrés Milla <andressmilla@gmail.com>
2019-05-18 16:09:50 -0300

    Agregué la forma de cambiar de usuario en la demo

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,8 +1,15 @@
 from django.views.generic import TemplateView
-
-# Create your views here.
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login
+from accounts.models import CustomUser
 
 
 class HomePageView(TemplateView):
 
     template_name = 'home.html'
+
+    def post(self, request, *args, **kwargs):
+        # usuario = CustomUser.objects.get(username='messi')
+        # login(request, usuario)
+        return HttpResponseRedirect(reverse('home'))
