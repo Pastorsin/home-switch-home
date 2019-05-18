5daf74d73b3ec4a105187f8b37f581d0e480edcf
Andrés Milla <andressmilla@gmail.com>
2019-05-18 15:29:46 -0300

    Agregué la modificación de Premium en admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,14 +5,17 @@ from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
 
-
 class CustomUserAdmin(UserAdmin):
 
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    fieldsets = (
+        (('User'), {
+            'fields': ('username', 'password', 'email', 'is_staff', 'edad', 'es_premium')
+        }),
+    )
     list_display = ['email', 'username', 'edad', 'is_staff', 'es_premium', ]
-
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
 
     edad = models.PositiveIntegerField(
-            null=True,
-            blank=True
+        null=True,
+        blank=True
     )
     foto = models.URLField(
-            null=True,
-            blank=True
-    ) 
+        null=True,
+        blank=True
+    )
     es_premium = models.BooleanField(
-            default=False
+        default=False
     )
 
     def cambiar_categoria(self):
