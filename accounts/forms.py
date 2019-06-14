7792598f8443ce1ab4684f17b184bfb40bf06907
Leonel Mandarino <leonelmandarino@gmail.com>
2019-06-14 18:06:29 -0300

    Agregada herencia de formularios del registro

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,7 +7,7 @@ from datetime import date
 from .models import CustomUser
 
 
-class CustomUserCreationForm(UserCreationForm):
+class CustomUserForm:
 
     def clean_fecha_nacimiento(self):
         data = self.cleaned_data['fecha_nacimiento']
@@ -17,22 +17,15 @@ class CustomUserCreationForm(UserCreationForm):
             raise ValidationError('Lo sentimos, para utilizar esta pagina debe ser mayor de edad')
         return data
 
+class CustomUserCreationForm(UserCreationForm, CustomUserForm):
+
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('first_name', 'last_name', 'email', 'foto', 'fecha_nacimiento')
+        fields = ('first_name', 'last_name', 'email', 'foto', 'fecha_nacimiento', 'dni')
 
 
-class CustomUserChangeForm(UserChangeForm):
-
-    def clean_fecha_nacimiento(self):
-        data = self.cleaned_data['fecha_nacimiento']
-        today = date.today()
-        diferencia_de_años = today.year - data.year - ((today.month, today.day) < (data.month, data.day))
-        if diferencia_de_años < 18:
-            raise ValidationError('Modificación de edad invalida, debe ser mayor a 18 años')
-        return data
+class CustomUserChangeForm(UserChangeForm, CustomUserForm):
 
     class Meta:
         model = CustomUser
-        fields = ('first_name', 'last_name', 'email', 'foto', 'fecha_nacimiento')
-
+        fields = ('first_name', 'last_name', 'email', 'foto', 'fecha_nacimiento', 'dni')
