6025ef97e4b31352b7da5fb648b6a3e251ec763f
Leonel Mandarino <leonelmandarino@gmail.com>
2019-06-15 17:15:09 -0300

    Agregada estructura basica del modificar perfil

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 
-from .views import SignUpView
-from .views import DetallePerfilView
-
+from .views import SignUpView, EditProfileView, DetallePerfilView
 
 urlpatterns = [
         path('signup/', SignUpView.as_view(), name='signup'),
-        path('verPerfil/<int:pk>/',
-         DetallePerfilView.as_view(),
-         name='verPerfil')
+        path('verPerfil/<int:pk>/', DetallePerfilView.as_view(), name='verPerfil'),
+        path('<int:pk>/edit/', EditProfileView.as_view(), name='profile_edit'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,11 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DetailView
 
 from .forms import CustomUserCreationForm
 from .models import CustomUser
 
 
 class SignUpView(CreateView):
-
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
@@ -15,3 +14,10 @@ class SignUpView(CreateView):
 class DetallePerfilView(DetailView):
     model = CustomUser
     template_name = 'verPerfil.html'
+
+
+class EditProfileView(UpdateView):
+    model = CustomUser
+    template_name = 'user_edit.html'
+    fields = ('first_name', 'last_name', 'email', 'foto', 'fecha_nacimiento', 'dni')
+    success_url = reverse_lazy('home')
