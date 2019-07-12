f9977b8b7bcbed6c514424026d272c8ee3ba396e
Mauro <molinamauro2@gmail.com>
2019-07-12 12:30:31 -0300

    Agregada la pagina de preguntas frecuentes

--- apps/estaticas/urls.py
+++ apps/estaticas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import PremiumView
+from .views import AyudaView
 
 urlpatterns = [
     path('premium', PremiumView.as_view(), name='premium_info'),
+    path('ayuda', AyudaView.as_view(), name='ayuda'),
 ]
--- apps/estaticas/views.py
+++ apps/estaticas/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 class PremiumView(TemplateView):
 
     template_name = 'premium_info.html'
+
+class AyudaView(TemplateView):
+	
+	template_name = 'ayuda.html'
