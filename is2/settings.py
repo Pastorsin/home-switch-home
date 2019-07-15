5bf12d1fa1a18e5fb834eb01c222235971525ff8
pastorsin <andressmilla@gmail.com>
2019-07-15 09:39:39 -0300

    Agregué el módulo locale para que muestre los días de la semanas en español

--- is2/settings.py
+++ is2/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import sys
 import django_heroku
+import locale
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -118,6 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
+locale.setlocale(locale.LC_TIME, "es_AR.UTF-8")
 
 LANGUAGE_CODE = 'es-es'
 
