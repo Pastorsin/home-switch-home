e35be665d7054d19d6f4b5c8126268220e4a51d7
Andrés Milla <andressmilla@gmail.com>
2019-08-05 00:17:54 -0300

    Agregué un contador de solicitudes en las notificaciones

--- static/base/notificaciones.js
+++ static/base/notificaciones.js
@@ -1,17 +1,29 @@
 class Notificador {
 
     constructor(sonido, selector) {
-        this.seNotifico = false
-        this.sonidoNotificacion = sonido
-        this.selector = selector
+        this.solicitudes = 0;
+        this.seNotifico = false;
+        this.sonidoNotificacion = sonido;
+        this.selector = selector;
     }
 
     notificar(cantidadNotificaciones) {
         if ((cantidadNotificaciones > 0) && (!this.seNotifico)) {
-            this.sonidoNotificacion.play();
-        	this.seNotifico = true
-            this.agregarCantidad(cantidadNotificaciones)
+        	this.seNotifico = true;
+            this.reproducirSonido();
+            this.incrementarSolicitudes();
+            this.agregarCantidad(cantidadNotificaciones);
         }
+    }
+
+    reproducirSonido() {
+    	if (this.solicitudes != 0) {
+    		this.sonidoNotificacion.play()
+    	}
+    }
+
+    incrementarSolicitudes() {
+    	this.solicitudes += 1;
     }
 
     agregarCantidad(cantidad) {
@@ -20,8 +32,8 @@ class Notificador {
     }
 
     leerNotificaciones() {
-        this.vaciarCantidad()
-        this.seNotifico = false
+        this.seNotifico = false;
+        this.vaciarCantidad();
     }
 
     vaciarCantidad() {
@@ -33,16 +45,16 @@ class Notificador {
 
 // Inicializaciones
 
-const SONIDO_NOTIFICACION = new Audio(SONIDO_NOTIFICACION_URL)
-const SELECTOR_NOTIFICADOR = $('#notificador')
-const NOTIFICADOR = new Notificador(SONIDO_NOTIFICACION, SELECTOR_NOTIFICADOR)
+const SONIDO_NOTIFICACION = new Audio(SONIDO_NOTIFICACION_URL);
+const SELECTOR_NOTIFICADOR = $('#notificador');
+const NOTIFICADOR = new Notificador(SONIDO_NOTIFICACION, SELECTOR_NOTIFICADOR);
 
-const XHTTP_LECTOR = new XMLHttpRequest()
-const XHTTP_NOTIFICADOR = new XMLHttpRequest()
+const XHTTP_LECTOR = new XMLHttpRequest();
+const XHTTP_NOTIFICADOR = new XMLHttpRequest();
 XHTTP_NOTIFICADOR.onreadystatechange = function() {
-        cantidadNotificaciones = this.responseText
-        NOTIFICADOR.notificar(cantidadNotificaciones)
-    }
+        cantidadNotificaciones = this.responseText;
+        NOTIFICADOR.notificar(cantidadNotificaciones);
+    };
 
 
 // Eventos
@@ -55,6 +67,6 @@ setInterval(function() {
 
 $("#campana").click(function() {
 	XHTTP_LECTOR.open("GET", "/leerNotificaciones");
-    XHTTP_LECTOR.send()
+    XHTTP_LECTOR.send();
     NOTIFICADOR.leerNotificaciones();
 });
